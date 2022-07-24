e8c3d7c6db2e82ef2217da3f93e90bffc3a33c6d
Alex S R <alexsr400@gmail.com>
2022-07-24 22:09:12 +0530

    Example for Gojs integration with Angular, with data binding and data update with startTransaction and commitTransaction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiagramComponent } from './components/diagram/diagram.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DiagramComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as go from 'gojs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gojs-app';
+  selectedNode?: go.Part;
+  model: go.Model = new go.TreeModel([
+    {
+      'key': 1, 'name': 'Satya Nadella', 'title': 'CEO'
+    },
+    {
+      'key': 2, 'name': 'Alex S R', 'title': 'Delivery Head', 'parent': 1
+    },
+    {
+      'key': 3, 'name': 'Sooraj', 'title': 'Delivery Head', 'parent': 1
+    }
+  ]);
+  formData = {
+    name: null
+  }
+  nodeSelected(node: go.Part) {
+    this.selectedNode = node || undefined;
+    this.formData.name = this.selectedNode?.data?.name || '';
+  }
+  save() {
+    this.model.startTransaction();
+    this.model.set(this.selectedNode?.data, 'name', this.formData.name);
+    this.model.commitTransaction();
+  }
 }
